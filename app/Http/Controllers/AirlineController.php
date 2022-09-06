7067f2012b5ac8ef0fa695bb57f2ec68dc72c75c
Mateo Goldwasser <mateo.goldwasser@lightit.io>
2022-09-06 15:20:01 -0300

    feat: made table layout

--- app/Http/Controllers/AirlineController.php
+++ app/Http/Controllers/AirlineController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Airline;
 use Illuminate\Http\Request;
 
 class AirlineController extends Controller
@@ -13,7 +14,7 @@ class AirlineController extends Controller
      */
     public function index()
     {
-        return view('AirlinesAdmin',[
+        return view('Airlines',[
             'airlines' => Airline::all()
         ]);
     }
--- resources/views/Airlines.blade.php
+++ resources/views/Airlines.blade.php
@@ -1,1 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+
+<body>
+<div class="px-4">
+    <table class='bg-red-200 px-4'>
+
+        <tr class="border-2 border-black">
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Cantidad de Vuelos que llegan</th>
+            <th>Cantidad de Vuelos que salen</th>
+            <th>Editar o eliminar ciudad</th>
+        </tr>
+
+        <tr>
+            <th>Prueba</th>
+            <th>Prueba</th>
+            <th>Prueba</th>
+            <th>Prueba</th>
+            <th>Prueba</th>
+        </tr>
+
+    </table>
+</body>
+</html>
