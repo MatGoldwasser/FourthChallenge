db5e6db6563b297bc55f75648cc50c5e90d2f2ec
Mateo Goldwasser <mateo.goldwasser@lightit.io>
2022-09-06 17:54:09 -0300

    feat: error message, finished add airline

--- resources/views/Airlines.blade.php
+++ resources/views/Airlines.blade.php
@@ -46,6 +46,11 @@
 
         <input type="submit" value="Submit"
                class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
+
+        @error("name")
+        <strong>No se puede dejar el nombre vacio ni introducir un nombre ya existente</strong>
+        @enderror
+
     </form>
 </div>
 </body>
