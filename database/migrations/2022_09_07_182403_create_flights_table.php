d7a83db9be85491d02e53c694ab4fd67cc7c68bc
Mateo Goldwasser <mateo.goldwasser@lightit.io>
2022-09-07 15:43:34 -0300

    feat: flight migration done

---
+++ database/migrations/2022_09_07_182403_create_flights_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\City;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Airline::class, 'airline');
+            $table->foreignIdFor(City::class, 'origin_id');
+            $table->foreignIdFor(City::class, 'arrival_id');
+            $table->dateTime('takeoff_time');
+            $table->dateTime('arrival_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+};
