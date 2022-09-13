880cde41fd09598e8bd0864565fb91bcf0b7d5f4
Mateo Goldwasser <mateo.goldwasser@lightit.io>
2022-09-13 14:59:42 -0300

    i did not realize I was working in main branch

---
+++ resources/views/components/flight-table.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Airline</th>
+            <th>Origin</th>
+            <th>Arrival</th>
+            <th>Takeoff time</th>
+            <th>Arrival time</th>
+        </tr>
+
+
+        @foreach($flights as $flight)
+            <tr id="flight-{{$flight->id}}"></tr>
+            <td>{{$flight->id}}</td>
+            <td>{{$flight->airline}}</td>
+            <td>{{$flight->origin_id}}</td>
+            <td>{{$flight->arrival_id}}</td>
+            <td>{{$flight->takeoff_time}}</td>
+            <td>{{$flight->arrival_time}}</td>
+        @endforeach
+    </table>
+</div>
